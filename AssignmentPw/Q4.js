1bf5c48f333d0839a1d1108bb03b9c2967aab480
HP <awadhrajpatel95@gmail.com>
2024-01-25 15:34:33 +0530

    first commit

---
+++ AssignmentPw/Q4.js
@@ -0,0 +1,8 @@
+/**
+ * Q7. De'cribe the u'a,e of the comma operator in JavaScript and provide an example.
+ */
+
+var a = 1, b = 2, c =3;
+console.log(a + b)
+console.log(b + c);
+console.log(a + b + c);
